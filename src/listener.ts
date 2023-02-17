f1273fbd066f7774981e99c678bdb50cf531a9ff
Dboee <Davidboee@gmail.com>
2023-02-17 13:54:52 +0100

    inits project with publisher and listner

---
+++ src/listener.ts
@@ -0,0 +1,14 @@
+import nats from 'node-nats-streaming';
+
+const stan = nats.connect('microservice-tutorial', 'listener', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe('ticket:created');
+  subscription.on('message', (msg) => {
+    console.log('Message received');
+  });
+});

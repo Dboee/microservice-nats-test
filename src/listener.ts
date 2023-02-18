2feee7f6e8d8c539a13db6c4eff5fe1659dbc4f1
Dboee <davidboee@gmail.com>
2023-02-18 10:09:20 +0100

    adds solution setup test

--- src/listener.ts
+++ src/listener.ts
@@ -1,19 +1,48 @@
 import nats, { Message } from 'node-nats-streaming';
 
+import { randomBytes } from 'crypto';
+
 console.clear();
 
-const stan = nats.connect('microservice-tutorial', 'listener', {
-  url: 'http://localhost:4222',
-});
+const stan = nats.connect(
+  'microservice-tutorial',
+  randomBytes(4).toString('hex'),
+  {
+    url: 'http://localhost:4222',
+  }
+);
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    // Set the manual acknowledgement mode
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('order-service');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'queue-group-name',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
-    console.log(
-      `Received event #${msg.getSequence()}, with data: ${msg.getData()}`
-    );
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    // Acknowledge the message
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
